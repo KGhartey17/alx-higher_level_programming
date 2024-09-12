e35353f47f26a9b8a06e64ce2815325f27d6301e
JK Ghartey <kaphaelghartey@gmail.com>
2024-09-12 12:34:19 -0700

    102

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,20 @@
+#!/usr/bin/python3
 def magic_calculation(a, b):
-    add, sub = magic_calculation_102.add, magic_calculation_102.sub
+    """Perform calculations based on the comparison of a and b.
+
+    Args:
+        a (int): The first integer.
+        b (int): The second integer.
+
+    Returns:
+        int: The result of the calculation.
+    """
+    from magic_calculation_102 import add, sub
 
     if a < b:
         c = add(a, b)
-
         for i in range(4, 6):
             c = add(c, i)
-
         return c
-
-    return sub(a, b)
+    else:
+        return sub(a, b)
