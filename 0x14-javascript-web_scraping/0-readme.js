265a9066ac9071230dab14e2a4b89cfd7da10e56
ALX Jerome <kaphaelghartey@gmail.com>
2025-02-11 14:58:46 -0800

    comma

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,20 +1,21 @@
 #!/usr/bin/node
 
-const fs = require("fs");
+const fs = require('fs');
 
 // Get the file path from the command-line arguments
 const filePath = process.argv[2];
 
 if (!filePath) {
-  console.error("Usage: ./0-readme.js <file_path>");
+  console.error('Usage: ./0-readme.js <file_path>');
   process.exit(1);
 }
 
 // Read the file asynchronously in UTF-8 encoding
-fs.readFile(filePath, "utf-8", (err, data) => {
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     console.error(err);
   } else {
     console.log(data);
   }
 });
+
