399c9ecce3e301a537d38d5672e9f243f154ef36
ALX Jerome <kaphaelghartey@gmail.com>
2025-02-11 15:23:57 -0800

    fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,20 +2,10 @@
 
 const fs = require('fs');
 
-// Get the file path from the command-line arguments
-const filePath = process.argv[2];
-
-if (!filePath) {
-  console.error('Usage: ./0-readme.js <file_path>');
-  process.exit(1);
-}
-
-// Read the file asynchronously in UTF-8 encoding
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
-    console.log(data);
+    process.stdout.write(data);
   }
 });
-
